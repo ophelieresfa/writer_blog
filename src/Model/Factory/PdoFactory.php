e4b2d187bf504d69e987610ef2866f4d4ed69674
Ophélie <resfa.ophelie@yahoo.fr>
2020-02-04 19:50:18 +0100

    Add content for the file PdoFactory.php

---
+++ src/Model/Factory/PdoFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Model\Factory;
+
+use PDO;
+
+class PdoFactory
+{
+    private static $pdo = null;
+
+    public static function getPDO()
+    {
+        require_once 'config/db.php';
+
+        if (self::$pdo === null) {
+            self::$pdo = new PDO(DB_DSN,DB_USER,DB_PASSWORD);
+            self::$pdo->exec('SET NAMES UTF8');
+        }
+
+        return self::$pdo;
+    }
+}
