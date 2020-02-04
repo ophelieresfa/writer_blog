07dcd75e95038845a4b2621f27501fda501ace88
Ophélie <resfa.ophelie@yahoo.fr>
2020-02-04 17:01:35 +0100

    Add content for the file Router.php

--- src/Router.php
+++ src/Router.php
@@ -5,9 +5,58 @@ namespace App;
 class Router
 {
 
-    public function run()
+    const PATH_CONTROLLER = 'App\Controller\\';
+    const HOME_CONTROLLER = 'HomeController';
+    const METHOD_CONTROLLER = 'controllerMethod';
+
+    private $controller = self::HOME_CONTROLLER;
+    private $method = self::METHOD_CONTROLLER;
+
+    public function __construct()
     {
+        $this->parseUrl();
+        $this->setController();
+        $this->setMethod();
+    }
+
+    public function parseUrl()
+    {
+        $action = filter_input(INPUT_GET, 'action');
+
+        if (!isset($action)) {
+            $action = 'home';
+        }
+
+        $action = explode('!', $action);
+        $this->controller = $action[0];
+        $this->method = count($action) == 1 ? 'controller' : $action[1];
+    }
+
+    public function setController()
+    {
+        $this->controller = ucfirst(strtolower($this->controller)) . 'Controller';
+        $this->controller = self::PATH_CONTROLLER . $this->controller;
+
+        if (!class_exists($this->controller)) {
+            $this->controller = self::PATH_CONTROLLER . self::HOME_CONTROLLER;
+        }
 
     }
 
+    public function setMethod()
+    {
+        $this->method = strtolower($this->method) . 'Method';
+
+        if (!method_exists($this->controller, $this->method)) {
+            $this->method = self::METHOD_CONTROLLER;
+        }
+    }
+
+    public function run()
+    {
+        $this->controller = new $this->controller();
+        $response = call_user_func([$this->controller, $this->method]);
+
+        echo filter_var($response);
+    }
 }
