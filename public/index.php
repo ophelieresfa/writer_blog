82e722c43b6a4bfc1d5b576260e945d7030efe0a
Ophélie <resfa.ophelie@yahoo.fr>
2020-02-25 15:27:37 +0100

    Update the file index.php
    Use session_start()

--- public/index.php
+++ public/index.php
@@ -5,6 +5,10 @@ use Tracy\Debugger;
 
 require_once '../vendor/autoload.php';
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 Debugger::enable();
 
 $router = new Router();
