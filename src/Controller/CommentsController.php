ab6d8740871dd57269f7f8cb252056a3bbb26d55
Ophélie <resfa.ophelie@yahoo.fr>
2020-02-25 15:28:27 +0100

    Update the file CommentsController.php
    Change variables name

--- src/Controller/CommentsController.php
+++ src/Controller/CommentsController.php
@@ -9,23 +9,24 @@ class CommentsController extends MainController
 
     public function controllerMethod()
     {
-        $lastBillet = ModelFactory::getModel('Billets')->listData();
-        $lastComment = ModelFactory::getModel('Commentaires')->listData();
+        $billets = ModelFactory::getModel('Billets')->listData();
+        $comments = ModelFactory::getModel('Commentaires')->listData();
 
-        return $this->twig->render('comments.twig', [
-            'lastBillet' => $lastBillet,
-            'lastComment' => $lastComment
+        return $this->twig->render('allChapters.twig', [
+            'billets' => $billets,
+            'comments' => $comments
         ]);
     }
 
     public function readMethod()
     {
-        $lastBillet = ModelFactory::getModel('Billets')->readData($this->get['id']);
-        $lastComment = ModelFactory::getModel('Commentaires')->listData($this->get['id'], 'id_billet');
+        $billet = ModelFactory::getModel('Billets')->readData($this->get->getGetVar('id'));
+        $comments = ModelFactory::getModel('Commentaires')->listData($this->get->getGetVar('id'), 'id_billet');
 
-        return $this->twig->render('comments.twig', [
-            'lastBillet' => $lastBillet,
-            'lastComment' => $lastComment
+        return $this->twig->render('allChapters.twig', [
+            'billet' => $billet,
+            'comments' => $comments
         ]);
+
     }
 }
