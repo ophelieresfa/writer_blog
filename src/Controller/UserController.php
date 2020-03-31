a8b6cdcd627743d8123afeba84bc6949d9962669
Ophélie <resfa.ophelie@yahoo.fr>
2020-03-31 19:46:12 +0200

    Update the files AdminController.php and UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -67,4 +67,10 @@ class UserController extends MainController
         }
         return $this->twig->render('subscribe.twig');
     }
+
+    public function deleteMethod()
+    {
+        ModelFactory::getModel('Utilisateurs')->deleteData($this->get->getVar('id_user'));
+
+        return $this->twig->render('home.twig');    }
 }
