778bde3911a7c81a16e99a83c77420ba06f79e0b
Ophélie <resfa.ophelie@yahoo.fr>
2020-04-16 20:08:30 +0200

    Add php doc for Extention.php

--- src/Controller/Extention/Extention.php
+++ src/Controller/Extention/Extention.php
@@ -7,24 +7,34 @@ use Twig\TwigFunction;
 
 /**
  * Class Extention
- * @package App\Controller
+ * @package App\Controller\Extention
  */
-
 class Extention extends AbstractExtension
 {
+    /**
+     * @var mixed|null
+     */
     private $session = null;
-    private $user = null;
+    /**
+     * @var |null
+     */
+    private $user    = null;
 
+    /**
+     * Extention constructor.
+     */
     public function __construct()
     {
         $this->session = filter_var_array($_SESSION);
 
-        if (isset($this->session['user']))
-        {
+        if (isset($this->session['user'])){
             $this->user = $this->session['user'];
         }
     }
 
+    /**
+     * @return array|TwigFunction[]
+     */
     public function getFunctions()
     {
         return array(
@@ -35,26 +45,41 @@ class Extention extends AbstractExtension
         );
     }
 
+    /**
+     * @param string $page
+     * @param array $params
+     * @return string
+     */
     public function url(string $page, array $params = [])
     {
         $params['action'] = $page;
+
         return 'index.php?' . http_build_query($params);
     }
 
+    /**
+     * @return bool
+     */
     public function hasFlash() {
         return empty($this->session['flash']) == false;
     }
 
+    /**
+     *
+     */
     public function typeFlash()
     {
-        if (isset($this->session['flash'])) {
+        if (isset($this->session['flash'])){
             echo $this->session['flash']['type'];
         }
     }
 
+    /**
+     *
+     */
     public function messageFlash()
     {
-        if (isset($this->session['flash'])) {
+        if (isset($this->session['flash'])){
             echo $this->session['flash']['message'];
             unset($_SESSION['flash']);
         }
